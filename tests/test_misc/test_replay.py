d9dcc6c133d9a199dbe106978e9aa2e9ce2ea589
glipR <37640160+glipR@users.noreply.github.com>
2023-02-12 20:54:41 +1100

    Add more replay tests.

--- tests/test_misc/test_replay.py
+++ tests/test_misc/test_replay.py
@@ -3,7 +3,7 @@ from ed_utils.decorators import number
 
 from action import PaintAction, PaintStep
 from replay import ReplayTracker
-from layers import green, red
+from layers import blue, green, red, invert
 from grid import Grid
 
 class TestReplay(unittest.TestCase):
@@ -17,14 +17,8 @@ class TestReplay(unittest.TestCase):
         steps2 = [PaintStep((5, 5), red), PaintStep((4, 4), red)]
 
         replay = ReplayTracker()
-        replay.add_action(PaintAction(
-            steps1[:],
-            False
-        ))
-        replay.add_action(PaintAction(
-            steps2[:],
-            False
-        ))
+        replay.add_action(PaintAction(steps1[:]))
+        replay.add_action(PaintAction(steps2[:]))
         # Do the replay
         replay.start_replay()
         v1 = replay.play_next_action(grid)
@@ -36,6 +30,77 @@ class TestReplay(unittest.TestCase):
             step.redo_apply(control_grid)
 
         self.assertGridEqual(grid, control_grid)
+
+    @number("5.2")
+    def test_advanced(self):
+        grid = Grid(Grid.DRAW_STYLE_SEQUENCE, 10, 10)
+        control_grid = Grid(Grid.DRAW_STYLE_SEQUENCE, 10, 10)
+
+        steps1 = [PaintStep((4, 4), green), PaintStep((4, 5), green), PaintStep((5, 4), green)]
+        steps2 = [PaintStep((5, 5), red), PaintStep((4, 4), red)]
+        action3 = PaintAction([], is_special=True)
+        steps4 = [PaintStep((5, 5), blue)]
+
+        replay = ReplayTracker()
+        replay.add_action(PaintAction(steps1[:]))
+        replay.add_action(PaintAction(steps2[:]))
+        replay.add_action(action3)
+        replay.add_action(action3, is_undo=True) # Undo the special.
+        replay.add_action(PaintAction(steps4[:]))
+        # Do the replay
+        replay.start_replay()
+
+        # Normal
+        v1 = replay.play_next_action(grid)
+        v2 = replay.play_next_action(grid)
+        for step in steps1 + steps2:
+            step.redo_apply(control_grid)
+        self.assertGridEqual(grid, control_grid)
+
+        # Special
+        v3 = replay.play_next_action(grid)
+        action3.redo_apply(control_grid)
+        self.assertGridEqual(grid, control_grid)
+
+        # Undo Special
+        v4 = replay.play_next_action(grid)
+        action3.undo_apply(control_grid)
+        self.assertGridEqual(grid, control_grid)
+
+        # Final step
+        v5 = replay.play_next_action(grid)
+        for step in steps4:
+            step.redo_apply(control_grid)
+        self.assertGridEqual(grid, control_grid)
+
+        v6 = replay.play_next_action(grid)
+        self.assertEqual((v1, v2, v3, v4, v5, v6), (False, False, False, False, False, True), "Wrong value returned from play_next_action")
+
+    @number("5.3")
+    def test_multiple_replays(self):
+        grid = Grid(Grid.DRAW_STYLE_ADD, 10, 10)
+        control_grid = Grid(Grid.DRAW_STYLE_ADD, 10, 10)
+
+        steps = [PaintStep((4, 4), green), PaintStep((4, 5), green), PaintStep((5, 4), green)]
+        replay = ReplayTracker()
+
+        # Add action and play.
+        replay.add_action(PaintAction(steps[:]))
+        replay.start_replay()
+        replay.play_next_action(grid)
+        for step in steps:
+            step.redo_apply(control_grid)
+        self.assertGridEqual(grid, control_grid)
+        self.assertEqual(replay.play_next_action(grid), True) # Finished.
+
+        # And repeat process.
+        replay.add_action(PaintAction(steps[:]))
+        replay.start_replay()
+        replay.play_next_action(grid)
+        for step in steps:
+            step.redo_apply(control_grid)
+        self.assertGridEqual(grid, control_grid)
+        self.assertEqual(replay.play_next_action(grid), True) # Finished.
 
     def assertGridEqual(self, grid1: Grid, grid2: Grid):
         for x in range(len(grid1.grid)):
