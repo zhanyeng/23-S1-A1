66c8e7b08113e0b0715030bce28f98ef3adcbaf4
glipR <37640160+glipR@users.noreply.github.com>
2023-02-12 20:20:01 +1100

    Fix undo documentation.

--- undo.py
+++ undo.py
@@ -4,25 +4,29 @@ from grid import Grid
 
 class UndoTracker:
 
-    def add_action(self, action: PaintAction):
-        pass
+    def add_action(self, action: PaintAction) -> None:
         """
         Adds an action to the undo tracker.
 
         If your collection is already full,
         feel free to exit early and not add the action.
         """
+        raise NotImplementedError()
 
-    def undo(self, grid: Grid):
-        pass
+    def undo(self, grid: Grid) -> PaintAction|None:
         """
         Undo an operation, and apply the relevant action to the grid.
         If there are no actions to undo, simply do nothing.
-        """
 
-    def redo(self, grid: Grid):
-        pass
+        :return: The action that was undone, or None.
+        """
+        raise NotImplementedError()
+
+    def redo(self, grid: Grid) -> PaintAction|None:
         """
         Redo an operation that was previously undone.
         If there are no actions to redo, simply do nothing.
+
+        :return: The action that was redone, or None.
         """
+        raise NotImplementedError()
