17b797de3645959edfa0fa44553ae0548c7caaf6
glipR <37640160+glipR@users.noreply.github.com>
2023-02-12 20:24:40 +1100

    Improve replay documentation.

--- replay.py
+++ replay.py
@@ -4,17 +4,46 @@ from action import PaintAction
 class ReplayTracker:
 
 
-    def start_replay(self):
+    def start_replay(self) -> None:
+        """
+        Called whenever we should stop taking actions, and start playing them back.
+
+        Useful if you have any setup to do before `play_next_action` should be called.
+        """
         pass
 
-    def add_action(self, action: PaintAction, is_undo: bool=False):
+    def add_action(self, action: PaintAction, is_undo: bool=False) -> None:
+        """
+        Adds an action to the replay.
+
+        `is_undo` specifies whether the action was an undo action or not.
+        Special, Redo, and Draw all have this is False.
+        """
         pass
 
-    def play_next_action(self, grid):
-        pass
+    def play_next_action(self, grid) -> None:
         """
         Plays the next replay action on the grid.
         Returns a boolean.
             - If there were no more actions to play, and so nothing happened, return True.
             - Otherwise, return False.
         """
+        pass
+
+if __name__ == "__main__":
+    action1 = PaintAction([], is_special=True)
+    action2 = PaintAction([])
+
+    r = ReplayTracker()
+    # add all actions
+    r.add_action(action1)
+    r.add_action(action2)
+    r.add_action(action2, is_undo=True)
+    # Start the replay.
+    r.start_replay()
+    t1 = r.play_next_action() # action 1, special
+    t2 = r.play_next_action() # action 2, draw
+    t3 = r.play_next_action() # action 2, undo
+    f = r.play_next_action()  # False, nothing to do.
+    assert (t1, t2, t3, f) == (True, True, True, False)
+
