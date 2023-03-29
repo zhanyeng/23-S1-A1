a7348cd567f50de21d4cac3937fd5a1ed4ee90ea
LAU ZHAN YENG <120074234+zhanyeng@users.noreply.github.com>
2023-03-29 11:37:18 +0800

    Update grid.py

--- grid.py
+++ grid.py
@@ -24,7 +24,19 @@ class Grid:
 
         Should also intialise the brush size to the DEFAULT provided as a class variable.
         """
-        raise NotImplementedError()
+        if x < 0 or y < 0:
+            raise ValueError("Cannot be negative!")
+        self.row = x
+        self.col = y
+        self.brush_size = Grid.DEFAULT_BRUSH_SIZE
+        self.drawStyle = draw_style
+
+        self.grid = ArrayR(self.row)
+        for i in range(self.row):
+            self.grid[i] = ArrayR(self.row)
+            for j in range(self.col):
+                if self.drawStyle == Grid.DRAW_STYLE_ADD:
+                    self.grid[i][j] == AdditiveLayerStore()
 
     def increase_brush_size(self):
         """
@@ -32,7 +44,9 @@ class Grid:
         if the brush size is already MAX_BRUSH,
         then do nothing.
         """
-        raise NotImplementedError()
+        if self.brush_size < Grid.MAX_BRUSH:
+            self.brush_size += 1
+        raise Exception("Brush size is already reached the max limit")
 
     def decrease_brush_size(self):
         """
@@ -40,7 +54,9 @@ class Grid:
         if the brush size is already MIN_BRUSH,
         then do nothing.
         """
-        raise NotImplementedError()
+        if self.brush_size > Grid.MIN_BRUSH:
+            self.brush_size -= 1
+        raise Exception("Brush size reached the min limit")
 
     def special(self):
         """
